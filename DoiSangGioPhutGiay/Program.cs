65ddac1fa3e5c63e00084e8fc6cbdfa4c89c7571
Your Name <you@example.com>
2026-09-12 11:15:19 +0700

    Add DoiSangGioPhutGiay project

---
+++ DoiSangGioPhutGiay/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Nhập dữ liệu
+        Console.WriteLine("Nhập số giây:");
+        int totalSeconds = int.Parse(Console.ReadLine());
+        // Khai báo biến và xử lý dữ liệu     
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+        // Xuất kết quả
+        Console.WriteLine($"Kết quả: {hours} giờ, {minutes} phút, {seconds} giây");
+    }
+}
