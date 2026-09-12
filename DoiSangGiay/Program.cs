14620548bdf245fd5c83435e6d6d269fa832855c
Your Name <you@example.com>
2026-09-12 15:53:20 +0700

    Add DoiSangGiay project

---
+++ DoiSangGiay/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhập số giờ:");
+        int Gio = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhập số phút:");
+        int Phut = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhập số giây:");
+        int Giay = int.Parse(Console.ReadLine());
+        int TongGiay = Gio * 3600 + Phut * 60 + Giay;
+        Console.WriteLine("Tổng số giây là: " + TongGiay);
+    }
+
+}
