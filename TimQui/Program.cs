bb95250c764cf7b1960abcedd9d92f5da28120c9
Your Name <you@example.com>
2026-09-13 08:55:05 +0700

    Add TimQui project

---
+++ TimQui/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int thang;
+         Console.WriteLine("Nhập số tháng (1-12):");
+          thang = int.Parse(Console.ReadLine());
+         int quy=(thang-1)/3+1;
+         Console.WriteLine("Tháng {0} thuộc quý {1}", thang, quy);
+          
+    }
+ }  
+
+
